e9cc07f1fb97aa7dd0556328ae31c5db226182fc
Henrique AC <henriqueac00@gmail.com>
2025-10-04 18:06:12 -0300

    Django 3: Fornecedores models, Listview and template (listar)

--- agenda/settings.py
+++ agenda/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 INSTALLED_APPS += [
     'home',
+    'fornecedores', 
 ]
 
 MIDDLEWARE = [
---
+++ fornecedores/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import ListView
+
+from .models import Fornecedor
+
+class FornecedoresView(ListView):
+    model = Fornecedor
+    template_name = 'fornecedores.html'
+
+    def get_queryset(self):
+        buscar = self.request.GET.get('buscar')
+        qs = super(FornecedoresView, self).get_queryset()
+        if buscar:
+            return qs.filter(nome__icontains=buscar)
+        return qs
